be9c381cbb25f18f305025f3ad65b795301c9f68
Gianni Ridwan <99036552@mydavinci.nl>
2020-11-30 13:37:33 +0100

    Opdracht C gemaakt.

---
+++ OpdrachtC.js
@@ -0,0 +1,17 @@
+var tafel = [2,4,6,8];
+
+for(i=1;i<11;i++){
+	document.getElementById("twee").innerHTML += i + " x " + tafel[0] + " = " + (i * tafel[0]) + "<br>";
+}
+
+for(i=1;i<11;i++){
+	document.getElementById("vier").innerHTML += i + " x " + tafel[1] + " = " + (i * tafel[1]) + "<br>";
+}
+
+for(i=1;i<11;i++){
+	document.getElementById("zes").innerHTML += i + " x " + tafel[2] + " = " + (i * tafel[2]) + "<br>";
+}
+
+for(i=1;i<11;i++){
+	document.getElementById("acht").innerHTML += i + " x " + tafel[3] + " = " + (i * tafel[3]) + "<br>";
+}
